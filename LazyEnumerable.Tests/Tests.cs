ce5d89a62c14f9e3032e66cbc57ab1f1b7e9e7c2
Dipesh Chauhan <dipesh@chauhan.net.au>
2013-10-14 18:54:33 +1100

    Initial Commit.

---
+++ LazyEnumerable.Tests/Tests.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace LazyEnumerable.Tests
+{
+	public class Tests
+	{
+		[Test]
+		public void BasicIntRangeTest()
+		{
+			var lazyEnumerable = new LazyEnumerable<int> (10, (position, count) =>
+			{
+				if (position >= 100)
+					return new List<int> ();
+				
+				var result = Enumerable.Range (position, count).Select (i => i * 2).ToList ();
+				
+				return result;
+			});
+
+			var expectedPositionCounter = 0;
+			foreach (var item in lazyEnumerable)
+			{
+				var expected = expectedPositionCounter++ * 2;
+				Assert.AreEqual (expected, item);
+			}
+		}
+
+		[Test]
+		public void FinishReturningInNonMod0PositionTest()
+		{
+			var lazyEnumerable = new LazyEnumerable<int> (10, (position, count) =>
+			{
+				if (position >= 101)
+					return new List<int> ();
+
+				var result = Enumerable.Range (position, count).Select (i => i * 2).ToList ();
+
+				return result;
+			});
+
+			var expectedPositionCounter = 0;
+			foreach (var item in lazyEnumerable)
+			{
+				var expected = expectedPositionCounter++ * 2;
+				Assert.AreEqual (expected, item);
+			}
+		}
+
+		[Test]
+		public void FinishReturningWithNullResponseTest()
+		{
+			var lazyEnumerable = new LazyEnumerable<int> (10, (position, count) =>
+			{
+				if (position >= 100)
+					return null;
+
+				var result = Enumerable.Range (position, count).Select (i => i * 2).ToList ();
+
+				return result;
+			});
+
+			var expectedPositionCounter = 0;
+			foreach (var item in lazyEnumerable)
+			{
+				var expected = expectedPositionCounter++ * 2;
+				Assert.AreEqual (expected, item);
+			}
+		}
+
+		[Test]
+		public void ReturnNonFullCountInFirstRequestResponseTest()
+		{
+			var lazyEnumerable = new LazyEnumerable<int> (1000, (position, count) =>
+			{
+				if (position >= 100)
+					return null;
+
+				var result = Enumerable.Range (position, count).Select (i => i * 2).ToList ();
+
+				return result;
+			});
+
+			var expectedPositionCounter = 0;
+			foreach (var item in lazyEnumerable)
+			{
+				var expected = expectedPositionCounter++ * 2;
+				Assert.AreEqual (expected, item);
+			}
+		}
+	}
+}
---
+++ LazyEnumerable/LazyEnumerable.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LazyEnumerable
+{
+	public delegate IEnumerable<T> LazyEnumerableItemLoader<T>(int position, int take);
+
+	public class LazyEnumerable<T> : IEnumerable<T>
+	{
+		protected readonly LazyEnumerableItemLoader<T> LazyEnumerableItemLoader;
+		protected readonly int Take;
+
+		public LazyEnumerable(int take, LazyEnumerableItemLoader<T> lazyEnumerableItemLoader)
+		{
+			Take = take;
+			LazyEnumerableItemLoader = lazyEnumerableItemLoader;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			T[] currentPagedResults = null;
+			var position = 0;
+			var paging = true;
+
+			do
+			{
+				if (position % Take == 0)
+					currentPagedResults = (LazyEnumerableItemLoader (position, Take) ?? new T[] { }).ToArray();
+				position += Take;
+
+				paging = currentPagedResults.Count () == Take;
+
+				foreach (var item in currentPagedResults)
+					yield return item;
+			}
+			while(paging);
+		}
+	}
+}
